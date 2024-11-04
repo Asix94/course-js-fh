f340c72e2a46e22540a4a97511d3e63c0cd542ac
Carlos Ramos <carlosrt1994@gmail.com>
2024-11-04 17:04:13 +0100

    Laboratorio alternativa al if else

--- 01-fundamentos/assets/js/if-else.js
+++ 01-fundamentos/assets/js/if-else.js
@@ -23,3 +23,18 @@ if( dia === 0) {
 } else {
     console.log('No es lunes, martes o domingo');
 }
+
+// Sin usar If Else, o Switch, únicamente objeto
+dia = 6; // 0: Domingo, 1: Lunes, 2: Martes, 3: Miercoles, 4: Jueves, 5: Viernes, 6: Sabado
+const diasLetras1 = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miércoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sábado'
+}
+const diasLetras2 = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'];
+// dia de la semana
+console.log(diasLetras2[dia] || 'Día no definido');
