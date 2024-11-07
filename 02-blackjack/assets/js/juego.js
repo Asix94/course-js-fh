9cead56bbad8be3a75b6fc5fca118a84ab2f0a11
Carlos Ramos <carlosrt1994@gmail.com>
2024-11-07 17:17:48 +0100

    pedir carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+// Esta función crea un nuevo deck
 const crearDeck = () => {
     
     for(let i = 2; i <= 10; i++) {
@@ -23,7 +24,6 @@ const crearDeck = () => {
         }
     }
 
-    console.log(deck); 
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
@@ -31,3 +31,21 @@ const crearDeck = () => {
 }
 
 crearDeck();
+
+// Esta función me permite tomar una carta
+
+const pedirCarta = () => {
+
+    if(deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop(); 
+
+    console.log(deck) // 
+    console.log(carta) // carta debe de ser de la baraja
+    return carta;
+
+}
+
+// pedirCarta();
